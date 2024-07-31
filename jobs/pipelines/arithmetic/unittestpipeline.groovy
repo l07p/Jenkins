e3d47dfd7252cd343edddb31510fd26c1e896cb0
l07p <l07p@gmx.de>
2024-07-31 17:53:04 +0200

    Update unittestpipeline.groovy

--- jobs/pipelines/arithmetic/unittestpipeline.groovy
+++ jobs/pipelines/arithmetic/unittestpipeline.groovy
@@ -1,17 +1,7 @@
 // unittestpipeline.groovy
 
 def runTests() {
-    println "Running tests..."
-    def result = add(2, 3)
-    if (result == 5) {
-        println "Test Passed: 2 + 3 = 5"
-    } else {
-        println "Test Failed: 2 + 3 != ${result}"
-    }
-}
-
-def add(num1, num2) {
-    return num1 + num2
+    echo "Hello from simpleScript!"
 }
 
 // Return the script to make its methods accessible
