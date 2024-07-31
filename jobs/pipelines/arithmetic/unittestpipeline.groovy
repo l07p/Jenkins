e8819fb48d9c4c72c413f82074bac7726d9e64e8
l07p <l07p@gmx.de>
2024-07-31 17:12:49 +0200

    Update unittestpipeline.groovy

--- jobs/pipelines/arithmetic/unittestpipeline.groovy
+++ jobs/pipelines/arithmetic/unittestpipeline.groovy
@@ -1,35 +1,14 @@
 def runTests() {
-    pipeline {
-        agent any
-
-        stages {
-            stage('Unit Test') {
-                steps {
-                    script {
-                        def result = arithmeticPipeline('add', 2, 3)
-                        echo "Test Result: ${result}"
-                        
-                        // Generate JUnit report
-                        def junitReport = """
-                        <testsuite name='ArithmeticTests'>
-                            <testcase name='Addition Test'>
-                                ${result == 5 ? "" : "<failure message='Expected 5 but got ${result}'/>"}
-                                <system-out>
-                                    <![CDATA[ 2 + 3 = ${result} ]]>
-                                </system-out>
-                            </testcase>
-                        </testsuite>
-                        """
-                        writeFile file: 'addition-test.xml', text: junitReport
-                    }
-                }
-            }
-
-            stage('Publish Test Results') {
-                steps {
-                    junit '**/*.xml'
-                }
-            }
-        }
+    def result = add(2, 3)
+    if (result == 5) {
+        println "Test Passed: 2 + 3 = 5"
+    } else {
+        println "Test Failed: 2 + 3 != ${result}"
     }
 }
+
+def add(num1, num2) {
+    return num1 + num2
+}
+
+return this
