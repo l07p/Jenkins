576f65c4eb088c0d831f98f34e5c1ed55add58b1
l07p <l07p@gmx.de>
2024-08-19 15:37:23 +0200

    added test py file to check instert_bank_data.py model

---
+++ jobs/database/test_insert_bank_data.py
@@ -0,0 +1,10 @@
+# test_insert_bank_data.py
+
+from insert_bank_data import insert_bank_data
+
+# Test data
+bank_name = "cmdirect"
+bank_code = "BADEH"
+
+# Call the function with test data
+insert_bank_data(bank_name, bank_code)
